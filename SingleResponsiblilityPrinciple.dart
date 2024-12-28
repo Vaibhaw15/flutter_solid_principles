ee96b084d551e909665dd8ebffb7dae4fee7c168
Vaibhaw Soni <vaibhawsoni900@gmail.com>
2024-12-28 13:53:06 +0530

    SingleResponsibility done!

---
+++ SingleResponsiblilityPrinciple.dart
@@ -0,0 +1,11 @@
+class AuthManager{
+   bool authenticateUser(String user,String password){
+     return true;
+   }
+}
+
+class profileManager{
+  void updateUserProfile(String userName, Map<String,dynamic>){
+    print('User profile updated for $user');
+   }
+}
